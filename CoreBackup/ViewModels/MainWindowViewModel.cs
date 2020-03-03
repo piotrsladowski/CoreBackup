077ad40d9ea89424296bbf3aba14d818fc42c19d
Piotr Śladowski <piotrsladowski7@gmail.com>
2020-03-03 21:01:03 +0100

    init

---
+++ CoreBackup/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBackup.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
