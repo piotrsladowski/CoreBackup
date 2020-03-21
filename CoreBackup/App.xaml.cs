b2c5b0bcec1a3cf5baa422727a158c5a8fe883db
Piotr Śladowski <piotrsladowski7@gmail.com>
2020-03-21 17:38:29 +0100

    Code refactor

--- CoreBackup/App.xaml.cs
+++ CoreBackup/App.xaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using CoreBackup.ViewModels;
 using CoreBackup.Views;
+using CoreBackup.Windows;
 
 namespace CoreBackup
 {
