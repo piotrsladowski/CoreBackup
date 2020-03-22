04f5a47eb0fe57e2189948cba4312d9496b5561a
Piotr Śladowski <piotrsladowski7@gmail.com>
2020-03-22 21:52:02 +0100

    DataGrid

--- CoreBackup/Models/IO/BasicIO.cs
+++ CoreBackup/Models/IO/BasicIO.cs
@@ -7,16 +7,15 @@ namespace CoreBackup.Models.IO
 {
     class BasicIO
     {
-        public List<string> getFilesInDirectory(string path)
+        public IEnumerable<FileInfo> getFilesInDirectory(string path)
         {
-            List<string> fNames = new List<string>();
             DirectoryInfo di = new DirectoryInfo(path);
-            foreach (var f in di.EnumerateFiles())
-            {
-                fNames.Add(f.ToString());
-            }
+            //foreach (FileInfo f in di.EnumerateFiles())
+            //{
+            //    fNames.Add(f.ToString());
+            //}
 
-            return fNames;
+            return di.EnumerateFiles();
         }
     }
 }
---
+++ CoreBackup/Models/IO/FileInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBackup.Models.IO
+{
+    class FileInformation
+    {
+        public string filename { get; set; }
+        public long size { get; set; }
+        private long _modificationTime;
+        public long modificationTime {
+            get {
+                return _modificationTime;
+            }
+            set {
+                _modificationTime = value;
+            }
+        }
+    }
+}
