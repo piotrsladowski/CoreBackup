9ce074b65dfe78468019bcc875c7a75e42dcfb5d
Mateusz Piotr <m.smendowski@gmail.com>
2020-04-05 17:22:33 +0200

    Navbar Help Option
    Opens Git Repository Project pressing Help menu item .  404 error -> Project is currently private.

--- CoreBackup/ViewModels/NavbarViewModel.cs
+++ CoreBackup/ViewModels/NavbarViewModel.cs
@@ -3,13 +3,46 @@ using Avalonia.Controls.ApplicationLifetimes;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Reactive;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace CoreBackup.ViewModels
 {
     class NavbarViewModel : ViewModelBase
     {
+        private ReactiveCommand<Unit, Unit> OpenGitWebsiteCommand { get; }
+        private void OpenGitWebsite()
+        {
+            string url = "https://github.com/piotrsladowski/CoreBackup";
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+
         public ReactiveCommand<Unit, Unit> OpenSettingsWindowCommand { get; }
         //public ReactiveCommand<Unit, Unit> ExitAppCommand { get; }
         public ReactiveCommand<Unit, Unit> OpenAboutWindowCommand { get; }
@@ -19,8 +52,10 @@ namespace CoreBackup.ViewModels
             OpenSettingsWindowCommand = ReactiveCommand.Create(OpenSettingsWindow);
             OpenAboutWindowCommand = ReactiveCommand.Create(OpenAboutWindow);
             ExitAppCommand = ReactiveCommand.Create(ExitApp);
+            OpenGitWebsiteCommand = ReactiveCommand.Create(OpenGitWebsite);
         }
 
+     
 
         private void OpenSettingsWindow()
         {
