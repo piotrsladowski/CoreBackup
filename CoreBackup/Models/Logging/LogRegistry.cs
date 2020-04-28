cdca2bf266a82ac9757c4341d9f5c82d1d368f79
Mateusz Piotr <m.smendowski@gmail.com>
2020-04-28 21:04:11 +0200

    EventLogView - Begginings
    - LogsData Class,
    - TODO : Responsive Grid inside Data Grid and Sortable Columns,

---
+++ CoreBackup/Models/Logging/LogRegistry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBackup.Models.Logging
+{
+    public class LogRegistry 
+    {
+        public int Id { get; set; }
+        public String Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public String Source { get; set; }
+        public String Importance { get; set; }
+        
+        public enum Priority
+        {
+            Low,
+            Medium,
+            High
+        }
+    }
+}
--- CoreBackup/ViewModels/EventLogViewModel.cs
+++ CoreBackup/ViewModels/EventLogViewModel.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using CoreBackup.Models.Local;
+using CoreBackup.Models.Logging;
+using ReactiveUI;
+using Serilog;
 
 namespace CoreBackup.ViewModels
 {
     class EventLogViewModel : ViewModelBase
     {
+        public static ObservableCollection<LogRegistry> LogsData { get; set; }
+        private static int id = 0;
+       
+        public EventLogViewModel()
+        {
+            
+            LogsData = new ObservableCollection<LogRegistry>();
+            //AddRegistry(rg);
+            AddNewRegistry("OKOKOKO", "OKOKOKO");
+           
+
+        }
+
+        public void AddRegistry()
+        {
+            //LogsData.Add();
+        }
+
+        private static void AddNewRegistry(string content, string source)
+        {
+            id += 1;
+            LogsData.Add(new LogRegistry(){Id = id, Content = content});
+        }
     }
 }
